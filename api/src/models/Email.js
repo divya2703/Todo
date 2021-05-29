4f95cc123ef213973de01ad4f51e1c9fbcb779bc
Divya Kumari <divya27@Divyas-MacBook-Air.local>
2021-05-29 21:27:59 +0530

    Added Email Schema

---
+++ api/src/models/Email.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
+const emailSchema = Schema({
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        required: 'Email address is required',
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    }
+});
+
+
+const Email = mongoose.model('Email', emailSchema)
+module.exports = Email
