a4214a5d846e96f10a307c62673d7df62c558b2c
Divya Kumari <divya27@Divyas-MacBook-Air.local>
2021-05-30 18:25:03 +0530

    Moved App.js code to a seperate Component Todo

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import './App.css';
+// import App from './App';
+import Todo from './components/Todo/Todo'
 import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Todo/>
   </React.StrictMode>,
   document.getElementById('root')
 );
